41f0cd68ac74b657a4fe8a306b4270812998fa0d
fdaniel13s <u20221c628@upc.edu.pe>
2025-05-11 20:10:17 -0500

    feat: REST interfaces configured to new feature about sowings and users.

--- WaruSmart.API/Crops/Interfaces/REST/Resources/CreateSowingResource.cs
+++ WaruSmart.API/Crops/Interfaces/REST/Resources/CreateSowingResource.cs
@@ -1,3 +1,3 @@
 ﻿namespace WaruSmart.API.Crops.Interfaces.REST.Resources;
 
-public record CreateSowingResource(int AreaLand,int CropId);
+public record CreateSowingResource(int AreaLand,int CropId, int UserId);
