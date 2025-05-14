f4865c7226d0fb4c2ae37c129d05c965c63f71ac
fdaniel13s <u20221c628@upc.edu.pe>
2025-05-14 13:02:58 -0500

    feat: add Device command and query services

---
+++ WaruSmart.API/Crops/Application/CommandServices/DeviceCommandService.cs
@@ -0,0 +1,34 @@
+﻿using WaruSmart.API.Crops.Domain.Model.Commands;
+using WaruSmart.API.Crops.Domain.Model.Entities;
+using WaruSmart.API.Crops.Domain.Repositories;
+using WaruSmart.API.Crops.Domain.Services;
+using WaruSmart.API.Shared.Domain.Repositories;
+
+namespace WaruSmart.API.Crops.Application.CommandServices;
+
+public class DeviceCommandService(IDeviceRepository deviceRepository, IUnitOfWork unitOfWork,
+    ISowingRepository sowingRepository): IDeviceCommandService
+{
+    public async Task<Device?> Handle(CreateDeviceCommand command)
+    {
+        var sowing = sowingRepository.FindByIdAsync(command.sowingId);
+        if (sowing == null)
+        {
+            throw new Exception("Sowing not found");
+        }
+
+        try
+        {
+            var device = new Device(command, sowing.Result);
+            await deviceRepository.AddAsync(device);
+            await unitOfWork.CompleteAsync();
+            return device;
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Error creating device: " + e.Message);
+            throw;
+        }
+        
+    }
+}
---
+++ WaruSmart.API/Crops/Application/QueryServices/DeviceQueryService.cs
@@ -0,0 +1,14 @@
+﻿using WaruSmart.API.Crops.Domain.Model.Entities;
+using WaruSmart.API.Crops.Domain.Model.Queries;
+using WaruSmart.API.Crops.Domain.Repositories;
+using WaruSmart.API.Crops.Domain.Services;
+
+namespace WaruSmart.API.Crops.Application.QueryServices;
+
+public class DeviceQueryService(IDeviceRepository deviceRepository): IDeviceQueryService
+{
+    public async Task<IEnumerable<Device>> Handle(GetAllDevicesBySowingId query)
+    {
+        return await deviceRepository.FindBySowingIdAsync(query.sowingId);
+    }
+}
