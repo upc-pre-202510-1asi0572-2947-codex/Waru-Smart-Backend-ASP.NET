01ffa1b4bccfbaec329716fb786daafb02c2c6ad
Carlos Pingus <132234605+KazutoTR@users.noreply.github.com>
2025-05-14 14:55:24 -0500

    chore: Updated Create Crop Controller

--- WaruSmart.API/Crops/Interfaces/REST/Transform/CreateCropSourceCommandFromResourceAssembler.cs
+++ WaruSmart.API/Crops/Interfaces/REST/Transform/CreateCropSourceCommandFromResourceAssembler.cs
@@ -7,6 +7,6 @@ public static class CreateCropSourceCommandFromResourceAssembler
 {
     public static CreateCropCommand ToCommandFromResource(CreateCropResource resource)
     {
-        return new CreateCropCommand(resource.Name, resource.ImageUrl, resource.Description,resource.Diseases,resource.Pests, resource.Cares);
+        return new CreateCropCommand(resource.Name, resource.ImageUrl, resource.Description/*resource.Diseases,resource.Pests, resource.Cares*/);
     }
 }
