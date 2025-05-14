02698dd84ce1fc6d8d036e01cd72583b063d4ead
fdaniel13s <u20221c628@upc.edu.pe>
2025-05-14 13:03:24 -0500

    feat: implement SowingsDevicesController with device creation and retrieval endpoints

---
+++ WaruSmart.API/Crops/Interfaces/REST/Resources/CreateDeviceResource.cs
@@ -0,0 +1,8 @@
+﻿namespace WaruSmart.API.Crops.Interfaces.REST.Resources;
+
+public record CreateDeviceResource( 
+    string SensorType,
+    string Location,
+    string Status,
+    int SowingId
+);
